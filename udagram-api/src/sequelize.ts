f93863791fdf955dbc95ecbc54079259f590d635
Vignesh Srivatsan <vsrivatsan53@gmail.com>
2023-12-24 10:39:09 -0800

    initial commit with set up for api and front end, added aws rds encryption fix on sequelize.ts

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -10,4 +10,12 @@ export const sequelize = new Sequelize({
 
   'dialect': config.dialect,
   'storage': ':memory:',
+  // Added below according to https://knowledge.udacity.com/questions/999143
+  // Fixes SequelizeConnectionError as encryption cannot be disabled on AWS RDS
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // added for compatibility with RDS
+    }
+  }
 });
